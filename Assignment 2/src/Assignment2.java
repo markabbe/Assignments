81bf3fff039c4527550f81cc6d718ab45c7f5ba0
Joe Stanton <jsromejoe@gmail.com>
2015-09-20 23:17:15 -0400

    Created Assignment 2

---
+++ Assignment 2/src/Assignment2.java
@@ -0,0 +1,13 @@
+public class Assignment2 {
+    public static void main(String[] args) {
+        /**
+         * 1. Print out your name and a random message using both System.out.print and System.out.println.
+         * System.out.print("Hello ");
+         * System.out.println("World!");
+         */
+
+        /**
+         * 2. Print some ascii art using print and println
+         */
+    }
+}
